c204dcb965a5ecdd62d4bcc9e35ff3b1fb64a8d0
derf6060 <derf6060@derfnet.attlocal.net>
2024-04-23 21:17:01 -0500

    I'm now able to play with the database through django.

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Message
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World. You're at the crud index.")
+    #return HttpResponse("Hello, World. You're at the crud index.")
+    messages = Message.objects.order_by('id')
+    temp = loader.get_template("crud/index.html")
+    context = {
+        "messages": messages,
+    }
+    return HttpResponse(temp.render(context, request))
