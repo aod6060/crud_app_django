7c529a6a9d753d161c1b9e66eaea10464505bc0e
derf6060 <derf6060@derfnet.attlocal.net>
2024-04-23 22:48:00 -0500

    Added the ability to create new messages. Time for editing.

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+    path("newpost/", views.new_post, name="newpost"),
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from django.urls import reverse
 from django.template import loader
 
 from .models import Message
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest):
     #return HttpResponse("Hello, World. You're at the crud index.")
     messages = Message.objects.order_by('id')
     temp = loader.get_template("crud/index.html")
@@ -13,3 +14,17 @@ def index(request):
         "messages": messages,
     }
     return HttpResponse(temp.render(context, request))
+
+def new(request: HttpRequest):
+    temp = loader.get_template("crud/new.html")
+    context = {}
+    return HttpResponse(temp.render(context, request))
+
+def new_post(request: HttpRequest):
+    if request.POST.get("message") == "":
+        return HttpResponseRedirect("/new")
+    else:
+        temp = Message.objects.create()
+        temp.message = request.POST.get("message")
+        temp.save()
+        return HttpResponseRedirect("/")
